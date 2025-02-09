2febb6a6c74219161dfcd5ef3203b4af2026ba0c
Javier Gómez <7153989+javiergomezve@users.noreply.github.com>
2025-02-08 21:27:24 -0500

    Connection to db

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
 import express from "express";
 import colors from "colors";
 import morgan from "morgan";
+import { db } from "./config/db";
+
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log(colors.blue.bold("Database connection success"));
+    } catch (e) {
+        console.error(colors.red.bold(`Error connecting to db: ${e}`));
+    }
+}
+
+connectDB();
 
 const app = express();
 
